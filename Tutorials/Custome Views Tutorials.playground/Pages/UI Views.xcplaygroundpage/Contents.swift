b646f80bd64515aa63a144d986b048c7560f9d2b
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-14 15:47:48 -0700

    UIView Challenge 0.1 solved

---
+++ Tutorials/Custome Views Tutorials.playground/Pages/UI Views.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: [Previous](@previous)
+
+import UIKit
+import PlaygroundSupport
+
+let canvasFrame = CGRect(x: 0, y: 0, width: 400, height: 200)
+let canvas = UIView(frame: canvasFrame)
+canvas.backgroundColor = .white
+PlaygroundPage.current.liveView = canvas // make the canvas appear in the assistant editor
